2516907e30559de39970191cd66f76adb0782089
alanquigley-toast <alan.quigley@toasttab.com>
2021-05-21 16:36:59 +0100

    chore: add layout template

---
+++ __TEMPLATES__/banquet-layout-spa/template/src/app/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { BanquetCustomProps } from '@toasttab/banquet-types'
+
+export function App(props: BanquetCustomProps) {
+  return (
+    <div className='p-4 text-heading-1 text-color-link'>
+      Start your SPA here
+    </div>
+  )
+}
