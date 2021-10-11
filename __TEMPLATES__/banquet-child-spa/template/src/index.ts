363c77770f931ec8b572d34ae591b0c958f501bb
alanquigley-toast <54547753+alanquigley-toast@users.noreply.github.com>
2021-10-11 18:20:32 +0100

    Update index.ts

--- __TEMPLATES__/banquet-child-spa/template/src/index.ts
+++ __TEMPLATES__/banquet-child-spa/template/src/index.ts
@@ -14,4 +14,4 @@ const reactLifecycles = banquetSingleSpaReact({
 export const bootstrap = reactLifecycles.bootstrap
 export const mount = reactLifecycles.mount
 export const unmount = reactLifecycles.unmount
-export const name = {{name}}
+export const name = '{{name}}'
