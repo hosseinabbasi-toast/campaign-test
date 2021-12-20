8d253ff7e748f7fe284e79d7d765885ed0ca9307
Keith Madden <keith.madden@toasttab.com>
2021-12-20 12:24:11 +0000

    feat(banquet-config): Remove custom webpack config (#35)
    Existing config was for duplicated file outputs to assist with SPA migration. That is no longer the default state so this config should not be provided out of the box
    
    This means SPA bundles based on this template will output only a bundle.js when built

--- __TEMPLATES__/base-template/webpack.config.js
+++ __TEMPLATES__/base-template/webpack.config.js
@@ -1,19 +1,7 @@
 /**
- * This is a TEMP requirement - it will output two bundles
- * - 'bundle.js'
- * - 'app.banquet.js'
- * If you need to edit this file - react out to the WEX team.
+ * Extend the default webpack configuration here
  */
 
-module.exports = () => [
-  {
-    output: {
-      filename: 'bundle.js'
-    }
-  },
-  {
-    output: {
-      filename: 'app.banquet.js'
-    }
-  }
-]
+module.exports = () => {
+  return {}
+}
